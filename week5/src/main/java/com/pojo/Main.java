1d64c71560518b3026cfc4e0c87a834a94ad9a2e
Eldar <ealtymyshov2@gmail.com>
2021-10-02 00:01:17 +0600

    Review tasks of week 5 (00:00 02.10.21)

--- week5/src/main/java/com/pojo/Main.java
+++ week5/src/main/java/com/pojo/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             String username = "root";
-            String password = "Eldar2002";
+            String password = "root";
             String url = "jdbc:mysql://localhost:3306/test";
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
