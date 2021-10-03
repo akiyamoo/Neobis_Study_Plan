12a62c4569b07c13d94d67218c641c1cb5f8b15e
Eldar <ealtymyshov2@gmail.com>
2021-10-03 18:39:22 +0600

    Review tasks of week 5 (18:38 03.10.21)

--- week5/src/main/java/com/pojo/Main.java
+++ week5/src/main/java/com/pojo/Main.java
@@ -4,55 +4,196 @@ import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String username = "root"; // USERNAME
+    private static final String password = "root"; // PASSWORD
+    private static final String url = "jdbc:mysql://localhost:3306/test"; // URL database
+    private static Connection connection;
+
+    public static void main(String[] args) throws SQLException{
+
         try {
-            String username = "root";
-            String password = "root";
-            String url = "jdbc:mysql://localhost:3306/test";
-            Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
-
-            statement.executeUpdate(
-                    "INSERT customers(customer_id, customer_first_name, customer_last_name) " +
-                            "VALUES (1, 'Eldar', 'Altymyshov')"
-            );
-
-            statement.executeUpdate(
-                    "INSERT customers(customer_id, customer_first_name, customer_last_name) " +
-                            "VALUES (2, 'Natasha', 'Sila')"
-            );
-
-            System.out.println("Insert:");
-
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM customers");
-
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
-            }
-
-            statement.executeUpdate("UPDATE customers SET customer_last_name = 'Grigorievna' WHERE customer_last_name = 'Sila'");
-
-            resultSet = statement.executeQuery("SELECT * FROM customers");
-
-            System.out.println("Update:");
-
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
-            }
-
-            statement.executeUpdate("DELETE FROM customers WHERE customer_id = 2");
-
-            resultSet = statement.executeQuery("SELECT * FROM customers");
-
-            System.out.println("Delete:");
-
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
-            }
-
+            connection = DriverManager.getConnection(url, username, password);
         }
-        catch (SQLException e) {
-            e.printStackTrace();
+        catch (SQLException e)
+        {
+            System.out.println("Error connect to database.");
+            System.out.println(e);
+            return;
         }
+
+        System.out.println("INSERT:");
+        insert("Vanya", "Petrashov");
+        insert("Igor", "Seleznov");
+        insert("Elena", "Akoeva");
+        //example
+        System.out.println();
+
+        System.out.println("SELECT:");
+        selectAll();
+        System.out.println();
+
+        System.out.println("UPDATE first_name:");
+        updateFirstName("Vanya", "Sanya");
+        System.out.println();
+
+        System.out.println("SELECT:");
+        selectAll();
+        System.out.println();
+
+        System.out.println("UPDATE last_name:");
+        updateLastName("Seleznov", "Borodino");
+        System.out.println();
+
+        System.out.println("SELECT:");
+        selectAll();
+        System.out.println();
+
+        System.out.println("DELETE id:");
+        deleteById(1);
+        deleteById(2);
+        System.out.println();
+
+        System.out.println("SELECT:");
+        selectAll();
+
     }
+
+    public static void insert(String firstName, String lastName) {
+
+        PreparedStatement statementInsert;
+
+        try {
+
+            statementInsert = connection.prepareCall("" +
+                    "INSERT customers(customer_first_name, customer_last_name) " +
+                    "VALUES (?, ?)");
+
+            //statementInsert.setInt(1, id);
+            statementInsert.setString(1, firstName);
+            statementInsert.setString(2, lastName);
+
+            statementInsert.executeUpdate();
+
+            statementInsert.close();
+
+            System.out.println("Complete method - insert()");
+
+        }
+        catch (SQLException e){
+            System.out.println("Error - method insert(). Error: " + e);
+        }
+
+    }
+
+    public static void selectAll(){
+
+        Statement statementSelect;
+        ResultSet resultSet;
+
+        try {
+            statementSelect = connection.createStatement();
+            // create statement connection to database
+
+            resultSet = statementSelect.executeQuery("SELECT * FROM customers"); // getting result from the statement
+
+            while (resultSet.next()){
+                String id = resultSet.getString("customer_id");
+                String firstName = resultSet.getString("customer_first_name");
+                String lastName = resultSet.getString("customer_last_name");
+                System.out.println(id + " " + firstName + " " + lastName);
+            }
+
+            statementSelect.close(); // close statement connection to database
+            resultSet.close(); // close result thread
+
+            System.out.println("Complete method - selectAll()");
+
+        }
+        catch (SQLException e){
+            System.out.println("Error - method selectAll(). Error: " + e);
+        }
+
+    }
+
+    public static void updateFirstName(String oldFirstName,String newFirstName) {
+
+        PreparedStatement statementUpdate;
+
+        try {
+
+            statementUpdate = connection.prepareCall("" +
+                    "UPDATE customers " +
+                    "SET customer_first_name = ?" +
+                    "WHERE customer_first_name ?");
+
+            statementUpdate.setString(2, oldFirstName);
+            statementUpdate.setString(1, newFirstName);
+
+            statementUpdate.executeUpdate();
+
+            statementUpdate.close();
+
+            System.out.println("Complete method - updateFirstName()");
+
+        }
+        catch (SQLException e){
+            System.out.println("Error - method updateFirstName(). Error: " + e);
+        }
+
+    }
+
+    public static void updateLastName(String oldLastName,String newLastName) {
+
+        PreparedStatement statementUpdate;
+
+        try {
+
+            statementUpdate = connection.prepareCall("" +
+                    "UPDATE customers " +
+                    "SET customer_last_name = ?" +
+                    "WHERE customer_last_name ?");
+
+            statementUpdate.setString(2, oldLastName);
+            statementUpdate.setString(1, newLastName);
+
+            statementUpdate.executeUpdate();
+
+            //statementUpdate.executeUpdate("UPDATE customers SET customer_last_name = 'Grigorievna' WHERE customer_last_name = 'Sila'");
+
+            statementUpdate.close();
+
+            System.out.println("Complete method - updateLastName()");
+
+        }
+        catch (SQLException e){
+            System.out.println("Error - method updateLastName(). Error: " + e);
+        }
+
+    }
+
+    public static void deleteById(int id) {
+
+        PreparedStatement statementDelete;
+
+        try {
+
+            statementDelete = connection.prepareCall("" +
+                    "DELETE FROM customers " +
+                    "WHERE customer_id = ?");
+
+            statementDelete.setInt(1, id);
+
+            statementDelete.executeUpdate();
+
+            statementDelete.close();
+
+            System.out.println("Complete method - deleteById(). Delete id = " + id);
+
+        }
+        catch (SQLException e){
+            System.out.println("Error - method deleteById(). Error: " + e);
+        }
+
+    }
+
 }
